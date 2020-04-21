7b8d8f998ee341a0595369b1cd3331a9ef9b9f90
ztxs <62808603+ztxs@users.noreply.github.com>
2020-04-21 11:11:09 +0300

    Create AdvancedSettings.lua

---
+++ AdvancedSettings.lua
@@ -0,0 +1,16 @@
+---------- Print
+
+print ("AdvancedSettings has been loaded")
+
+---------- Update
+
+local CURRENTVERSION = "1.0"
+local LATESTVERSION = http.Get("https://raw.githubusercontent.com/ztxs/updater/master/AdvancedSettings.txt")
+local function Update() 
+    if CURRENTVERSION ~= LATESTVERSION then
+        currentScript = file.Open(GetScriptName(), "w")
+        currentScript:Write(http.Get("https://raw.githubusercontent.com/LunarLuzogSM/OriLua/master/OriLua.lua"))
+        currentScript:Close()
+        LoadScript(GetScriptName())
+    end
+end
