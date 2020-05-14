8e084a1fab767b10cbb0f4405bb2add840f888f1
ztxs <62808603+ztxs@users.noreply.github.com>
2020-05-14 17:53:41 +0300

    Update StrixLua.lua

--- StrixLua.lua
+++ StrixLua.lua
@@ -1,1 +1,22 @@
+-- StrixLua
+-- Made by Zetix
+
+------------------------------------------------ Print
+
+print("StrixLua has been successfully loaded")
+
+------------------------------------------------ Auto Update
+
+local CURRENTVERSION = "1.0"
+local LATESTVERSION = http.Get("https://raw.githubusercontent.com/ztxs/updater/master/StrixLua.txt")
+local function Update()
+    if CURRENTVERSION ~= LATESTVERSION then
+        currentScript = file.Open(GetScriptName(), "w")
+        currentScript:Write(http.Get("https://raw.githubusercontent.com/ztxs/updater/master/StrixLua.lua"))
+        currentScript:Close
+        LoadScript(GetScriptName())
+    end
+end
+
+--------------------------------------------- Gui buttons etc
 
