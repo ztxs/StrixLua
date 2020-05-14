bfc0deb46ac785fc5b043b01710d639d2650c3b5
ztxs <62808603+ztxs@users.noreply.github.com>
2020-05-14 18:32:56 +0300

    Update StrixLua.lua

--- StrixLua.lua
+++ StrixLua.lua
@@ -1,23 +1,24 @@
 -- StrixLua
 -- Made by Zetix
 
------------------------------------------------- Print
+-------------------------------------- Print
 
 print("StrixLua has been successfully loaded")
 
------------------------------------------------- Auto Update
+------------------------------------ Auto Update
 
-local CURRENTVERSION = "1.0"
-local LATESTVERSION = http.Get("https://raw.githubusercontent.com/ztxs/updater/master/StrixLua.txt")
-local function Update()
-    if CURRENTVERSION ~= LATESTVERSION then
-        currentScript = file.Open(GetScriptName(), "w")
-        currentScript:Write(http.Get("https://raw.githubusercontent.com/ztxs/updater/master/StrixLua.lua"))
-        currentScript:Close
-        LoadScript(GetScriptName())
-    end
-end
 
---------------------------------------------- Gui buttons etc
 
-local StrixLua_TAB = gui.Tab(gui.Reference("Misc"), "strix.ll", "StrixLua")
+------------------------------------ Gui buttons etc
+
+local StrixLua_TAB = gui.Tab(gui.Reference("Misc"), "strix.1", "StrixLua")
+
+
+local StrixUpdate_TAB = gui.Tab(gui.Reference("Misc"), "strix.2", "StrixUpdate")
+
+-------------- Update 1
+
+local StrixLua_UPDATER_GBOX = gui.Groupbox(StrixUpdate_TAB, "Updater", 10, 10, 160, 0)
+
+
+local StrixLua_CHANGELOG_GBOX = gui.Groupbox(StrixUpdate_TAB, "Changelog", 190, 10, 290, 0)
