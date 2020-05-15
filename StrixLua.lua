2aed90e12626622081af743c101bd0069a1c19c0
ztxs <62808603+ztxs@users.noreply.github.com>
2020-05-15 16:28:01 +0300

    Update StrixLua.lua

--- StrixLua.lua
+++ StrixLua.lua
@@ -7,7 +7,7 @@ print("StrixLua has been successfully loaded")
 
 ------------------------------------ Auto Update
 
-local CURRENTVERSION = "1.1"
+local CURRENTVERSION = "1.0"
 local LATESTVERSION = http.Get("https://raw.githubusercontent.com/ztxs/updater/master/StrixLua.txt")
 local function Update() 
     if CURRENTVERSION ~= LATESTVERSION then
@@ -294,3 +294,78 @@ end
 callbacks.Register("Draw", "paint_traverse", paint_traverse)
 --End - HL Speed Indicator--
 --End - LUA--
+
+-- headmove
+
+local movement_ref = gui.Reference("MISC", "StrixLua", "Features")
+
+local sync_movement = gui.Checkbox(movement_ref, "sync_movement_0.", "Sync Head Movement", false)
+
+local s_w, s_h = draw.GetScreenSize()
+
+function syncMovement(cmd, pos)
+    local world_forward = {vector.Subtract( pos,  {entities.GetLocalPlayer():GetAbsOrigin().x, entities.GetLocalPlayer():GetAbsOrigin().y, entities.GetLocalPlayer():GetAbsOrigin().z} )}
+    local ang_LocalPlayer = {engine.GetViewAngles().x, engine.GetViewAngles().y, engine.GetViewAngles().z }
+    
+    cmd.forwardmove = ( ( (math.sin(math.rad(ang_LocalPlayer[2]) ) * world_forward[2]) + (math.cos(math.rad(ang_LocalPlayer[2]) ) * world_forward[1]) ) * 200 ) -- mine
+    cmd.sidemove = ( ( (math.cos(math.rad(ang_LocalPlayer[2]) ) * -world_forward[2]) + (math.sin(math.rad(ang_LocalPlayer[2]) ) * world_forward[1]) ) * 200 )
+end
+
+function is_movement_keys_down()
+    return input.IsButtonDown( 87 ) or input.IsButtonDown( 65 ) or input.IsButtonDown( 83 ) or input.IsButtonDown( 68 ) or input.IsButtonDown( 32 )
+end
+
+function is_crouching(player)
+    return player:GetProp('m_flDuckAmount') > 0.1
+end
+
+
+
+local is_synced = false
+
+callbacks.Register("CreateMove", function(cmd)
+    if not sync_movement:GetValue() then return end
+    succ, err = pcall(function() is_movement_keys_down() is_synced = false end)
+    if err or is_movement_keys_down() then return end
+    
+    local players = entities.FindByClass( "CCSPlayer" )
+    
+    for k, player in pairs(players) do
+        local player_pos = {player:GetAbsOrigin().x, player:GetAbsOrigin().y, player:GetAbsOrigin().z}
+        local distance = vector.Distance(player_pos, {entities.GetLocalPlayer():GetAbsOrigin().x, entities.GetLocalPlayer():GetAbsOrigin().y, entities.GetLocalPlayer():GetAbsOrigin().z})
+        
+        local z_dist = entities.GetLocalPlayer():GetAbsOrigin().z - player_pos[3]
+        
+        local d_min = 0
+        local d_max = 0
+        if not is_crouching(player) then
+            d_min = 70
+            d_max = 85
+        else
+            d_min = 50
+            d_max = 64
+        end
+        if (distance > d_min and distance < d_max) and (z_dist > d_min and z_dist < d_max) then
+            syncMovement(cmd, player_pos)
+            is_synced = true
+        else
+            is_synced = false
+        end
+    end        
+end)
+
+
+callbacks.Register("Draw", function(cmd)
+    if not sync_movement:GetValue() or not sync_movement_indicator:GetValue() then return end
+    local main_font = draw.CreateFont("Verdana", sync_movement_indicator_size:GetValue());
+    draw.SetFont(main_font)
+
+    draw.Color(255,0,0)
+    if is_synced then
+        draw.Color(0,255,0)
+        draw.Text(sync_movement_indicator_X:GetValue(),sync_movement_indicator_Y:GetValue(), "Synced")
+    else
+        draw.Text(sync_movement_indicator_X:GetValue(),sync_movement_indicator_Y:GetValue(), "Unsynced")
+    end
+    
+end) 
