f7ccf762f718239792383745f4b6e8e165a0138c
ztxs <62808603+ztxs@users.noreply.github.com>
2020-05-15 19:01:24 +0300

    Update StrixLua.lua

--- StrixLua.lua
+++ StrixLua.lua
@@ -44,16 +44,21 @@ local StrixLua_CHANGELOG_TEXT = gui.Text(StrixLua_CHANGELOG_GBOX, http.Get("http
 
 -- black windows
 
-local StrixLua_FUNCTIONS_GBOX = gui.Groupbox(StrixLua_TAB, "Features", 10, 10, 220, 0)
+local StrixLua_TROLLING_GBOX = gui.Groupbox(StrixLua_TAB, "Trolling", 10, 10, 220, 0)
 
 local StrixLua_MOVEMENT_GBOX = gui.Groupbox(StrixLua_TAB, "Movement", 250, 10, 190, 0)
+
+local StrixLua_APPEARANCE_GBOX = gui.Groupbox(StrixLua_TAB, "Appearance", 455, 10, 175, 0)
+
+
+
 -- features/functions
 
 
 -- grenade owner
-local active = gui.Checkbox( gui.Reference( 'Misc', 'StrixLua', 'Features' ), 'esp.world.grenadeowner', 'Grenade Owner', 0 )
+local active = gui.Checkbox( gui.Reference( 'Misc', 'StrixLua', 'Trolling' ), 'esp.world.grenadeowner', 'Grenade Owner', 0 )
 active:SetDescription('Shows owner of Smokes and Decoys')
-local cb = gui.Combobox( gui.Reference( 'Misc', 'StrixLua', 'Features' ), 'esp.world.grenadeowner.types', 'Grenades', 'Decoys', 'Smokes', 'Both' )
+local cb = gui.Combobox( gui.Reference( 'Misc', 'StrixLua', 'Trolling' ), 'esp.world.grenadeowner.types', 'Grenades', 'Decoys', 'Smokes', 'Both' )
 cb:SetDescription('Choose which Grenades should be shown')
 local FindByClass, GetLocalPlayer, Text, Color, WorldToScreen = entities.FindByClass, entities.GetLocalPlayer, draw.Text, draw.Color, client.WorldToScreen
 
@@ -159,8 +164,8 @@ callbacks.Register("Draw", SkyBox)
 ---- movement
 --Fake Scroll on JumpBug--
 --by uvxxvu - https://aimware.net/forum/thread-128377.html--
-local ui_checkbox = gui.Checkbox( gui.Reference("MISC","StrixLua", "Movement"),"misc.autojumpbug.scroll", "Fake Scroll On Jump-bug Miss", 1 )
-ui_checkbox:SetDescription( "Fakes scroll input if Auto Jump-Bug fails." )
+local ui_checkbox = gui.Checkbox( gui.Reference("MISC","StrixLua", "Movement"),"misc.autojumpbug.scroll", "Fake Scroll", 1 )
+ui_checkbox:SetDescription( "Fakes scroll if Auto Jump miss." )
 
 local function get_local_player( )
 
@@ -297,7 +302,7 @@ callbacks.Register("Draw", "paint_traverse", paint_traverse)
 
 -- headmove
 
-local movement_ref = gui.Reference("MISC", "StrixLua", "Features")
+local movement_ref = gui.Reference("MISC", "StrixLua", "Trolling")
 
 local sync_movement = gui.Checkbox(movement_ref, "sync_movement_0.", "Sync Head Movement", false)
 
@@ -382,5 +387,84 @@ end )
 -- vanilla knife
 
 local list_knife = {"bayonet", "css", "flip", "gut", "karambit", "m9_bayonet", "tactical", "falchion", "survival_bowie", "butterfly", "push", "cord", "canis", "ursus", "gypsy_jackknife", "outdoor", "stiletto", "widowmaker", "skeleton"};
-local ui_gb = gui.Groupbox(gui.Reference("Misc", "StrixLua"), "Vanilla Knife", 250, 240, 190); local ui_knife = gui.Combobox(ui_gb, "vanilla", "Knife Model", unpack(list_knife));
+local ui_gb = gui.Groupbox(gui.Reference("Misc", "StrixLua"), "Vanilla Knife", 250, 210, 190); local ui_knife = gui.Combobox(ui_gb, "vanilla", "Knife Model", unpack(list_knife));
 local ui_add = gui.Button(ui_gb, "Add", function() gui.Command(ui_knife:GetValue() == 0 and "skin.add weapon_bayonet" or "skin.add weapon_knife_" .. list_knife[(ui_knife:GetValue() + 1)]); end);
+
+
+
+-- door spam by stacky
+
+local KEYBOX = gui.Keybox(gui.Reference("Misc", "StrixLua", "Trolling"), "misc.doorspam", "Door Spam Key", 0)
+local switch = false
+
+callbacks.Register( "CreateMove", function(cmd)
+    if KEYBOX:GetValue() ~= 0 then
+        if input.IsButtonDown(KEYBOX:GetValue()) then
+            if switch then client.Command("+use", true)
+            else client.Command("-use", true) end
+            switch = not switch
+        else
+            if not switch then client.Command("-use", true) end
+        end
+    end
+end )
+
+
+-------------------- Zeusbot
+local zeusbot = gui.Checkbox(gui.Reference("Misc", "StrixLua", "Trolling"), "enable_zeusbot_chkbox", "Enable Zeusbot", 0)
+
+zeusbot:SetDescription("Automatically zeus")
+local function zeuslegit()
+if not zeusbot:GetValue() or entities.GetLocalPlayer() == nil then 
+return 
+end
+
+local Weapon = entities.GetLocalPlayer():GetPropEntity("m_hActiveWeapon") 
+if Weapon == nil then 
+return 
+end
+
+local CWeapon = Weapon:GetClass() 
+local trige, trigaf, trighc = gui.GetValue("lbot.trg.enable"), gui.GetValue("lbot.trg.autofire"), gui.GetValue("lbot.trg.zeus.hitchance")
+
+if trige ~= 1 and trigaf ~= 1 and trighc ~= gui.GetValue("rbot.accuracy.weapon.zeus.hitchance") then 
+trige2, trigaf2, trighc2 = gui.GetValue("lbot.trg.enable"), gui.GetValue("lbot.trg.autofire"), gui.GetValue("lbot.trg.zeus.hitchance") 
+end
+
+if CWeapon == "CWeaponTaser" then 
+gui.SetValue("lbot.trg.enable", 1) 
+gui.SetValue("lbot.trg.autofire", 1) 
+gui.SetValue("lbot.trg.zeus.hitchance", gui.GetValue("rbot.accuracy.weapon.zeus.hitchance"))
+else 
+gui.SetValue("lbot.trg.enable", trige2) 
+gui.SetValue("lbot.trg.autofire", trigaf2) 
+gui.SetValue("lbot.trg.zeus.hitchance", trighc2) 
+end 
+end
+
+callbacks.Register("Draw", zeuslegit)
+
+-- Rainbow Hud by stacky
+
+local REF = gui.Reference( "Misc", "StrixLua", "Appearance" )
+local CHECKBOX = gui.Checkbox( REF, "rainbowhud.enable", "Rainbow Hud", false )
+local SLIDER = gui.Slider( REF, "rainbowhud.interval", "Rainbow Hud Interval", 1, 0, 5, 0.05 )
+
+local color = 1
+local time = globals.CurTime()
+local orig = client.GetConVar( "cl_hud_color" )
+
+callbacks.Register( "Draw", function()
+    if CHECKBOX:GetValue() then
+        client.Command( "cl_hud_color " .. color, true )
+        if globals.CurTime() - SLIDER:GetValue() >= time then
+            color = color + 1
+            time = globals.CurTime()
+        end
+        if color > 9 then color = 1 end
+    end
+end )
+
+callbacks.Register( "Unload", function()
+    client.Command( "cl_hud_color " .. orig, true )
+end )
