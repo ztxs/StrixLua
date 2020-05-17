6ee75fce931802341a7e00bbcd71fa0bb3d14103
ztxs <62808603+ztxs@users.noreply.github.com>
2020-05-17 13:43:06 +0300

    Update StrixLua.lua

--- StrixLua.lua
+++ StrixLua.lua
@@ -7,7 +7,7 @@ print("StrixLua has been successfully loaded")
 
 ------------------------------------ Auto Update
 
-local CURRENTVERSION = "1.2"
+local CURRENTVERSION = "1.3"
 local LATESTVERSION = http.Get("https://raw.githubusercontent.com/ztxs/updater/master/StrixLua.txt")
 local function Update() 
     if CURRENTVERSION ~= LATESTVERSION then
